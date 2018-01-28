71ab93aae914e76289fb7b4e234813194e5a76d1
LukaszWiszniewski <wisznu07@gmail.com>
2018-01-28 20:30:34 +0100

    Added time formatter module

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -1,4 +1,5 @@
 var os = require('os');
+var time = require('../modules/timeFormatter.js');
 
 function getOSinfo() {
     var type = os.type();
@@ -16,7 +17,7 @@ function getOSinfo() {
     console.log('System:', type);
     console.log('Release:', release);
     console.log('CPU model: ', cpu);
-    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
+    console.log('Uptime: ~',time.formattedTime(uptime) );
     console.log('User name:', userInfo.username);
     console.log('Home dir:', userInfo.homedir);
 }
